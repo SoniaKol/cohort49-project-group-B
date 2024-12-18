755fc2089937b2395826274f925352d3cf49606e
azizsigar <aazizsigar@gmail.com>
2024-12-18 18:30:27 +0100

    add navigation links and available restaurants component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Nav from "./components/Nav";
 import Home from "./pages/Home/Home";
 import CreateUser from "./pages/User/CreateUser";
@@ -13,6 +13,9 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/user" element={<UserList />} />
         <Route path="/user/create" element={<CreateUser />} />
+        <Route path="/about-us" element={<h1>About Us</h1>} />
+        <Route path="/restaurants" element={<h1>Restaurants</h1>} />
+        <Route path="/logout" element={<h1>Logout</h1>} />
       </Routes>
     </>
   );
