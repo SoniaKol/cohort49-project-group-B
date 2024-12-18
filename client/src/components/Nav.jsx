d0b82a27fa0a0af71d7c73c29290a1b968f6c3fe
HackYourFuture <hackyourfuture@HackYourFutures-MacBook-Air.local>
2024-12-18 16:11:19 +0100

    feat: add initial OrderCart page structure and components

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -6,6 +6,10 @@ import TEST_ID from "./Nav.testid";
 const Nav = () => {
   return (
     <ul>
+      <nav>
+        <Link to="/order-cart">Order Cart</Link>
+      </nav>
+
       <Link to="/" data-testid={TEST_ID.linkToHome}>
         <li>Home</li>
       </Link>
---
+++ client/src/pages/OrderCarts/OrderCart.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import CartItem from "../../components/CartItem";
+
+const OrderCart = () => {
+  return (
+    <div>
+      <h1>Order Cart</h1>
+      <CartItem />
+    </div>
+  );
+};
+
+export default OrderCart;
