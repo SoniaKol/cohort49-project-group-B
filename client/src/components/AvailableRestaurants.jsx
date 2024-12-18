728cf5cc2e10c2d1f32b9607308750f63b0691fe
azizsigar <aazizsigar@gmail.com>
2024-12-18 18:30:27 +0100

    add navigation links and available restaurants component

---
+++ client/src/components/AvailableRestaurants.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AvailableRestaurants() {
+  return <div>AvailableRestaurants will show random pizza with pagination</div>;
+}
+
+export default AvailableRestaurants;
