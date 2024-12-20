0d28f45cd4c245ffa19536c77bef68690e1c6f66
azizsigar <aazizsigar@gmail.com>
2024-12-20 15:19:11 +0100

    add SearchBar component and integrate it into Home page

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 
 import AvailableRestaurants from "../../components/AvailableRestaurants";
 import TEST_ID from "./Home.testid";
+import SearchBar from "../../components/SearchBar";
 
 const Home = () => {
   return (
     <div data-testid={TEST_ID.container}>
       <h1>This is the homepage</h1>
       <AvailableRestaurants />
+      <SearchBar />
     </div>
   );
 };
