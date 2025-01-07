2069f7567252b2e8d7e9f441c2b7ca1376c67ae6
Nimasaghi-dev <nimasaghi.dev@gmail.com>
2025-01-07 17:48:25 +0100

    feat: complete backend for order-cart and fix prettier errors

--- server/src/db/connectDB.js
+++ server/src/db/connectDB.js
@@ -3,13 +3,6 @@ import mongoose from "mongoose";
 
 dotenv.config();
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGODB_URL);
-    console.log("Connected to MongoDB");
-  } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
-  }
-};
+const connectDB = () => mongoose.connect(process.env.MONGODB_URL);
 
 export default connectDB;
