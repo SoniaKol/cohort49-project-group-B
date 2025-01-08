fd1ff2a4a2e39a879d5d36cf6ebab27d30dabbab
azizsigar <aazizsigar@gmail.com>
2025-01-08 19:00:10 +0100

    refactor: replace hardcoded pizza list with dynamic restaurant data from server

--- client/src/components/AvailableRestaurants.jsx
+++ client/src/components/AvailableRestaurants.jsx
@@ -1,43 +1,8 @@
 import React, { useState } from "react";
 
+// Import the pizza data
+import restaurantData from "../../../server/src/data/restaurant.js"; // If the file is in the same folder
 // Example list of pizzas with names and image URLs
-const pizzaList = [
-  { name: "Pepperoni Pizza", imageUrl: "/images/pepperoni-pizza.jpg" },
-  { name: "Margherita Pizza", imageUrl: "/images/margherita-pizza.jpg" },
-  { name: "Hawaiian Pizza", imageUrl: "/images/hawaiian-pizza.jpg" },
-  { name: "BBQ Chicken Pizza", imageUrl: "/images/bbq-chicken-pizza.jpg" },
-  { name: "Vegetarian Pizza", imageUrl: "/images/vegetarian-pizza.jpg" },
-  { name: "Mushroom Pizza", imageUrl: "/images/mushroom-pizza.jpg" },
-  { name: "Four Cheese Pizza", imageUrl: "/images/four-cheese-pizza.jpg" },
-  { name: "Meat Lovers Pizza", imageUrl: "/images/meat-lovers-pizza.jpg" },
-  {
-    name: "Buffalo Chicken Pizza",
-    imageUrl: "/images/buffalo-chicken-pizza.jpg",
-  },
-  { name: "Supreme Pizza", imageUrl: "/images/supreme-pizza.jpg" },
-  { name: "Greek Pizza", imageUrl: "/images/greek-pizza.jpg" },
-  { name: "Pesto Pizza", imageUrl: "/images/pesto-pizza.jpg" },
-  { name: "Seafood Pizza", imageUrl: "/images/seafood-pizza.jpg" },
-  {
-    name: "Chicken Alfredo Pizza",
-    imageUrl: "/images/chicken-alfredo-pizza.jpg",
-  },
-  { name: "Cheeseburger Pizza", imageUrl: "/images/cheeseburger-pizza.jpg" },
-  { name: "Spicy Italian Pizza", imageUrl: "/images/spicy-italian-pizza.jpg" },
-  { name: "Pineapple Pizza", imageUrl: "/images/pineapple-pizza.jpg" },
-  {
-    name: "Cheese Stuffed Crust Pizza",
-    imageUrl: "/images/cheese-stuffed-crust-pizza.jpg",
-  },
-  {
-    name: "Philly Cheesesteak Pizza",
-    imageUrl: "/images/philly-cheesesteak-pizza.jpg",
-  },
-  {
-    name: "Spinach and Feta Pizza",
-    imageUrl: "/images/spinach-feta-pizza.jpg",
-  },
-];
 
 function AvailableRestaurants() {
   // Pagination states
@@ -45,12 +10,12 @@ function AvailableRestaurants() {
   const [currentPage, setCurrentPage] = useState(1);
 
   // Calculate total pages
-  const totalPages = Math.ceil(pizzaList.length / itemsPerPage);
+  const totalPages = Math.ceil(restaurantData.length / itemsPerPage);
 
   // Slice pizza list to get the current page items
   const indexOfLastItem = currentPage * itemsPerPage;
   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-  const currentPizzas = pizzaList.slice(indexOfFirstItem, indexOfLastItem);
+  const currentPizzas = restaurantData.slice(indexOfFirstItem, indexOfLastItem);
 
   // Handle page change
   const goToPage = (pageNumber) => {
